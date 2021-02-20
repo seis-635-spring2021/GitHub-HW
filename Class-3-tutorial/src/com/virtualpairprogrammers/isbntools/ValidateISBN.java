55723ece079f2bbc9c4d91ed8235aeed662f9e7e
Ryan Poorman <Ryan.Poorman@leidos.com>
2021-02-19 19:20:53 -0600

    End of Section #4 and #5

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,18 +2,54 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
+	private static final int LONG_ISBN_MULTIPLIER = 10;
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int LONG_ISBN_LENGTH = 13;
+
 	public boolean checkISBN(String isbn) {
-		int total = 0;
 
-		for (int i = 0; i < 10; i++) {
-			total += isbn.charAt(i) * (10 - i);
+		if (isbn.length() == LONG_ISBN_LENGTH) {
+			return isThisAValidLongISBN(isbn);
 		}
-
-		if (total % 11 == 0) {
-			return true;
-		} else {
-			return false;
+		else if (isbn.length() == SHORT_ISBN_LENGTH) {
+			return isThisAValidShortISBN(isbn);			
 		}
+		throw new NumberFormatException("ISBN numbers must be 10 or 13 digits long");
 	}
 
+	private boolean isThisAValidShortISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++)
+		{
+			if (!Character.isDigit(isbn.charAt(i))) {
+				if (i ==9 && isbn.charAt(i) == 'X') {
+					total += 10;
+				}
+				else {
+					throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+				}
+			}
+			else {
+				total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN_LENGTH -i);
+			}
+		}
+
+		return (total % SHORT_ISBN_MULTIPLIER == 0);
+	}
+
+	private boolean isThisAValidLongISBN(String isbn) {
+		int total = 0;
+		
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			if (i % 2 == 0) {
+				total += Character.getNumericValue(isbn.charAt(i));
+			}
+			else {
+				total += Character.getNumericValue(isbn.charAt(i)) * 3;
+			}
+		}
+		return (total % LONG_ISBN_MULTIPLIER == 0);
+	}
 }
--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -8,19 +8,54 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkAValidISBN() {
-		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0140449116");
+	public void checkAValid10DigitISBN() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		boolean result = isbnValidator.checkISBN("0140449116");
 		assertTrue("first value", result);
-		result = validator.checkISBN("0140177396");
+		result = isbnValidator.checkISBN("0140177396");
 		assertTrue("second value", result);
 	}
 
 	@Test
-	public void checkAnInvalidISBN() {
-		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0140449117");
+	public void checkAValid13DigitISBN() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		boolean result = isbnValidator.checkISBN("9781853260087");
+		assertTrue("first value", result);
+		result = isbnValidator.checkISBN("9781853267338");
+		assertTrue("second value", result);
+	}
+
+	@Test
+	public void TenDigitISBNNumbersEndingInAnXAreValid() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		boolean result = isbnValidator.checkISBN("012000030X");
+		assertTrue(result);
+	}
+
+	@Test
+	public void checkAnInvalid10DigitISBN() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		boolean result = isbnValidator.checkISBN("0140449117");
 		assertFalse(result);
+	}
+
+	@Test
+	public void checkAnInvalid13DigitISBN() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		boolean result = isbnValidator.checkISBN("9781853267336");
+		assertFalse(result);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nineDigitISBNsAreNotAllowed() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		isbnValidator.checkISBN("123456789");
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nonNumericISBNsAreNotAllowed() {
+		ValidateISBN isbnValidator = new ValidateISBN();
+		isbnValidator.checkISBN("helloworld");
 	}
 
 }
