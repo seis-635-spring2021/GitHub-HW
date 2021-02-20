f977d9fb4b49b25ff64fad4f483afba37efc3961
seis-635-spring2021 <seis.635.spring2021@gmail.com>
2021-02-19 20:22:03 -0600

    second TDD commit

--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -11,8 +11,19 @@ public class ValidateISBNTest
 	public void checkAValidISBN()
 	{
 		ISBNValidator validator = new ISBNValidator();
-		boolean result = validator.checkISBN(140449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("first ISBN", result);
+		
+		result = validator.checkISBN("0140177396");
+		assertTrue("second ISBN", result);
+	}
+
+	@Test
+	public void checkAnInvalidISBN()
+	{
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("0140449117");	
+		assertFalse(result);
 	}
 
 }
