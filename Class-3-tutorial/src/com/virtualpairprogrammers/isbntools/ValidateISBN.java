0f13f1e543b5c9d768712d1a7dc51e72ee0b68be
jcpalzerust <jcpalzer@.stthomas.edu>
2021-02-19 20:53:48 -0600

    Final Commit of TDD Tutorial

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,10 +1,33 @@
 package com.virtualpairprogrammers.isbntools;
 
-public class ValidateISBN {
+public class ValidateISBN 
+{
 
-	public boolean checkISBN(int isbn) {
-		// TODO Auto-generated method stub
-		return true;
+	public boolean checkISBN(String isbn) 
+	{
+		
+		if (isbn.length() != 10)
+		{
+			throw new NumberFormatException("ISBN numbers must be 10 digits");
+		}
+		int total = 0;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			if (!Character.isDigit(isbn.charAt(i)))
+			{
+				throw new NumberFormatException("ISBNs must be in number format");
+			}
+			total += isbn.charAt(i) * (10 - i);
+		}
+		
+		if (total % 11 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
-
 }
