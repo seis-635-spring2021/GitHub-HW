83459eb4464c196c291202ec5121b7bf6dbbc408
HanLuong13 <hluong13@gmail.com>
2021-02-19 19:33:21 -0600

    first TDD commit

---
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -0,0 +1,18 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidateISBNTest
+{
+
+	@Test
+	public void checkAValidISBN()
+	{
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
+	}
+
+}
