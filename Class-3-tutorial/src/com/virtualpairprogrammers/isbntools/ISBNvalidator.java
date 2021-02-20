b1d102628109a5179babab70d7165ae8b865b3f6
bpdoherty1 <54919600+bpdoherty1@users.noreply.github.com>
2021-02-19 20:53:29 -0600

    commit in class but have errors because i'm in jUnit5 so the need to workout the exception handling

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNvalidator.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNvalidator.java
@@ -2,9 +2,35 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ISBNvalidator {
 
-	public boolean checkISBN(int isbn) {
-		// TODO Auto-generated method stub
-		return true;
+	public boolean checkISBN(String isbn) {
+		
+		if (isbn.length() != 10) 
+		{
+			throw new NumberFormatException("Nope");
+			
+		}
+		
+		int total = 0;
+				
+			
+		
+		
+		
+				for (int i = 0; i<10; i++)
+				{
+					total += isbn.charAt(i) * (10 - i);
+					
+				}
+		
+				if (total % 11 == 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+				
 	}
 
 	
--- Class-3-tutorial/tests/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/tests/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -3,14 +3,42 @@ package com.virtualpairprogrammers.isbntools;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class ValidateISBNTest {
 
 	@Test
 	void checkValidISBN() {
 		ISBNvalidator validator = new ISBNvalidator();
-		boolean result = validator.checkISBN(140449116);
+		boolean result = validator.checkISBN("0140449116");
 		assertTrue(result);
+		
+		result = validator.checkISBN("0140177396");
+		assertTrue(result);
+	}
+	
+	@Test
+	void checkInvalidISBN() {
+		ISBNvalidator validator = new ISBNvalidator();
+		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
+	
+	@Test //need to fix this because is Junit 4 i'm using Junit5 (expected=NumberFormatException.class)
+	public void nineDigitISBNnotallowed()
+	{
+		ISBNvalidator validator = new ISBNvalidator();
+		boolean result = validator.checkISBN("123456789");
+		
+		
+		
+	}
+	
+	@Test
+	public void nonNumericISBNs()
+	{
+		fail();
+		
 	}
 
 }
