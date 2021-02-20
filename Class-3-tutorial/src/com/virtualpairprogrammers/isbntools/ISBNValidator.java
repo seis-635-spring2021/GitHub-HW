88ce0a27fd9e82c86fdb5a41ef82b90d7df9f310
Kevin Rotich <rtkevk@gmail.com>
2021-02-19 20:53:45 -0600

    final commit of TDD tutorial.

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNValidator.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNValidator.java
@@ -2,9 +2,32 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ISBNValidator {
 
-	public boolean checkISBN(int isbn) {
-		// TODO Auto-generated method stub
-		return true;
+	public boolean checkISBN(String isbn) {
+		
+		if(isbn.length() !=10) {
+			throw new NumberFormatException("ISBN Numbers must be 10 digits long");
+		}
+		
+		int total = 0;
+				
+		for (int i = 0; i < 10; i++) 
+		{
+			if (!Character.isDigit(isbn.charAt(i))) {
+				throw new NumberFormatException("ISBN numbers must be 10 digits");
+			}
+			
+			total += isbn.charAt(i)*(10 - i);
+		}
+		
+		if ((total % 11) == 0) 
+		{
+			return true;
+		}
+		
+		else 
+			return false;
+		
+		
 	}
 
 }
--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -9,8 +9,32 @@ public class ValidateISBNTest {
 	@Test
 	public void checkAValidISBN() {		
 		ISBNValidator validator = new ISBNValidator();
-		boolean result = validator.checkISBN(40449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("first value", result);
+		
+		
+		result = validator.checkISBN("0140177396");
+		assertTrue("second value", result);
+		
+	}
+	
+	@Test
+	public void checkAnInvalidISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
+	
+	@Test (expected = NumberFormatException.class)
+	public void nine_digit_ISBNs_are_not_allowed() {
+		ISBNValidator validator = new ISBNValidator();
+		validator.checkISBN("123456789");
+	}
+	
+	@Test (expected = NumberFormatException.class)
+	public void non_numeric_isbns_are_not_allowed() {
+		ISBNValidator validator = new ISBNValidator();
+		validator.checkISBN("hellow world");
 	}
 
 }
