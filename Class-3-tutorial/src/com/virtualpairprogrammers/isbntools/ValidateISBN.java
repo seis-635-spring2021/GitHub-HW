50fb583e127eaa83339e7e29d8994dc816de71dd
dang1840 <dang1840@stthomas.edu>
2021-02-19 20:28:16 -0600

    second TDD commit

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,9 +2,23 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(int ibsn) {
+	public boolean checkISBN(String isbn) {
+	
+		if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits long");
 		
-		return true;
+		int total = 0;
+		
+		for (int i = 0; i < 10; i++) {
+			if (!Character.isDigit(isbn.charAt(i))) throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+			total += isbn.charAt(i) * (10 - i);
+		}
+		
+		if (total % 11 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
-
+	
 }
