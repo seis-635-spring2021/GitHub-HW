a188964a0330c032d9c9e1488600c9df62df9967
HanLuong13 <hluong13@gmail.com>
2021-02-19 20:55:24 -0600

    third TDD commit

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -5,9 +5,19 @@ public class ValidateISBN
 
 	public boolean checkISBN(String isbn)
 	{
+		
+		if (isbn.length() != 10) 
+		{
+			throw new NumberFormatException("ISBN number must be 10 digits long.");
+		}
 		int total = 0;
+		
 		for (int i = 0; i < 10; i++)
 		{
+			if (!Character.isDigit(isbn.charAt(i)))
+			{
+				throw new NumberFormatException("ISBN numbers must be 10 digits.");
+			}
 			total += isbn.charAt(i) * (10 - i);
 		}
 
--- Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -13,19 +13,33 @@ public class ValidateISBNTest
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("first ISBN", result);
-		
-		result = validator.checkISBN("01400177396");
+
+		result = validator.checkISBN("0140177396");
 		assertTrue("second ISBN", result);
-		
+
 	}
-	
+
 	@Test
 	public void checkAnInvalidISBN()
 	{
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0140449116");
-		
+		boolean result = validator.checkISBN("0140449117");
+
 		assertFalse(result);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nineDigitISBNsAreNotAllowed()
+	{
+		ValidateISBN validator = new ValidateISBN();
+		validator.checkISBN("123456789");
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void nonNumericISBNsAreNotAllowed()
+	{
+		ValidateISBN validator = new ValidateISBN();
+		validator.checkISBN("helloworld");
 	}
 
 }
