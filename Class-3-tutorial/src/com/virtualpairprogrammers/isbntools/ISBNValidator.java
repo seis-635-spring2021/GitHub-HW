24c15b379386d807118c875c58995c95420c2e1b
JHSU <jhsu@stthomas.edu>
2021-02-19 20:53:50 -0600

    final commit of TDD tutorial

--- Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNValidator.java
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNValidator.java
@@ -5,9 +5,16 @@ public class ISBNValidator {
 
 	public boolean checkISBN(String isbn) {
 		
+		if (isbn.length() !=10) {
+			throw new NumberFormatException("ISBN numbers must be 10 digits");
+		}
+		
 		int total = 0;
 				
 		for (int i=0; i < 10; i++) {
+			if (!Character.isDigit(isbn.charAt(i))) {
+				throw new NumberFormatException("ISBN numbers must be 10 digits");
+			}
 			total += isbn.charAt(i) * (10 - i);
 		}
 		
