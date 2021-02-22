2dbacd6eb3a354c4a149486d70e15e795f8eed5b
fatimahalattas <alat1475@stthomas.edu>
2021-02-21 18:16:17 -0600

    first TDD commit

---
+++ Class-3-tutorial/src/com/virtualpairprogrammers/isbntools/ISBNValidator.java
@@ -0,0 +1,10 @@
+package com.virtualpairprogrammers.isbntools;
+
+public class ISBNValidator {
+
+	public boolean checkISBN(int isbn) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
