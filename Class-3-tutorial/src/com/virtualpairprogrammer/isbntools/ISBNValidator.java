692726f6acd81e74e4f9923c5d96929d84af4093
mq mala <malandula@icloud.com>
2021-02-24 21:34:15 -0600

    Second Commit TDD

--- Class-3-tutorial/src/com/virtualpairprogrammer/isbntools/ISBNValidator.java
+++ Class-3-tutorial/src/com/virtualpairprogrammer/isbntools/ISBNValidator.java
@@ -2,9 +2,18 @@ package com.virtualpairprogrammer.isbntools;
 
 public class ISBNValidator {
 
-	public boolean checkISBN(int isbn) {
-		// TODO Auto-generated method stub
-		return true;
+	public boolean checkISBN(String isbn) {
+		int total = 0;
+		
+		for (int i = 0; i < 10; i++) {
+			total += isbn.charAt(i) * (10-1);
+		}
+		
+		if (total % 11 == 0) {
+			return true;
+		}else {
+			return false;
+		}		
 	}
 
 }
--- Class-3-tutorial/test/com/virtualpairprogrammer/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammer/isbntools/ValidateISBNTest.java
@@ -9,10 +9,17 @@ public class ValidateISBNTest {
 	@Test
 	public void checkAValidISBN() {
 		ISBNValidator validator = new ISBNValidator();
-		boolean result = validator.checkISBN(140449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("First value", result);
+		result = validator.checkISBN("0140177396");
+		assertTrue("Second value", result);
 	}
 	
-	
+	@Test
+	public void checkAnInvalidISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
 
 }
